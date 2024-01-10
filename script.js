faf791ec305901a110bb783d654ccd2dbdfe5d54
Abhishek Anil <141896050+abshekanil@users.noreply.github.com>
2024-01-10 11:42:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your code here
+document.addEventListener('DOMContentLoaded', function(){
+	let table = document.getElementById('myTable');
+	let allPrice = document.querySelectorAll('td[data-ns-test="prices"]');
+	let sum = 0;
+	allPrice.forEach(function(element){
+		sum+=parseFloat(element.textContent.replace('$',''));
+	});
+
+	let newRows = table.insertRow(-1);
+	let cell1 = newRows.insertCell(0);
+
+	cell1.dataset.nsTest = "grandTotal";
+
+	cell1.innerHTML = 'Total price = '+ sum;
+});
