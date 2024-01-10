6532e31a94c9236846b86e61162eb928fd8bc6d5
Abhishek Anil <141896050+abshekanil@users.noreply.github.com>
2024-01-10 12:15:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,13 +4,19 @@ document.addEventListener('DOMContentLoaded', function(){
 	let allPrice = document.querySelectorAll('td[data-ns-test="price"]');
 	let sum = 0;
 	allPrice.forEach(function(element){
-		sum+=parseFloat(element.textContent.replace('$',''));
+		let price = parseFloat(element.textContent.replace('$',''));
+
+		if(!isNaN(price))
+		{
+			sum+=price;
+		}
 	});
+	
 
 	let newRows = table.insertRow(-1);
 	let cell1 = newRows.insertCell(0);
 
 	cell1.dataset.nsTest = "grandTotal";
 
-	cell1.innerHTML = 'Total price = '+ sum;
+	cell1.innerHTML = 'Total price = '+ sum.toFixed(2);
 });
